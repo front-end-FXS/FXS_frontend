3e8074e840ee10809cc09b9e628cd84efc8df466
ultravoz <goris00@gmail.com>
2015-07-13 10:25:34 +0200

    recent updates files
    base files updated by pablo, first commit

---
+++ website-js/site.js
@@ -0,0 +1,173 @@
+$(document).ready(function () {
+
+    // Layout - Menu 
+
+    var menuLeft = document.getElementById('cbp-spmenu-s1'),
+        menuRight = document.getElementById('cbp-spmenu-s2'),
+        listView = document.getElementById('cbp-spmenu-s3'),
+        showLeftPush = document.getElementById('showLeftPush'),
+        showRightPush = document.getElementById('showRightPush'),
+        showLeft = document.getElementById('showLeft'),
+        body = document.body;
+
+    showLeftPush.onclick = function () {
+        classie.toggle(this, 'active');
+        classie.toggle(body, 'cbp-spmenu-push-toright');
+        classie.toggle(menuLeft, 'cbp-spmenu-open');
+        $('.fxs_listView').removeClass('cbp-spmenu-open');
+        $('.fxs_toggleList').removeClass('active');
+        disableOther('showLeftPush');
+    };
+    showRightPush.onclick = function () {
+        classie.toggle(this, 'active');
+        classie.toggle(body, 'cbp-spmenu-push-toleft');
+        classie.toggle(menuRight, 'cbp-spmenu-open');
+        $('.fxs_listView').removeClass('cbp-spmenu-open');
+        $('.fxs_toggleList').removeClass('active');
+        disableOther('showRightPush');
+    };
+    showLeft.onclick = function () {
+        classie.toggle(this, 'active');
+        classie.toggle(listView, 'cbp-spmenu-open');
+        disableOther('showLeft');
+    };
+
+    function disableOther(button) {
+        if (button !== 'showLeftPush') {
+            classie.toggle(showLeftPush, 'disabled');
+        }
+        if (button !== 'showRightPush') {
+            classie.toggle(showRightPush, 'disabled');
+        }
+        if (button !== 'showLeft') {
+            classie.toggle(showLeft, 'disabled');
+        }
+    }
+
+    ////////// Stick'em ////////// Se activará para anuncios fijos en layout
+    // $('.fxs_listView').stickem({offset:80});
+
+    ////////// Buttons active state //////////
+    // Esto ESTÁ METIDO A SACO para que funcione el trigger de los botones
+    $('.fxs_overflowOptions').click(function(evt) {
+        $(this).toggleClass("active");
+        evt.stopPropagation();
+    });
+
+    $('.fxs_btn_dd').click(function(evt) {
+        $('.fxs_btn_group').toggleClass("active");
+        evt.stopPropagation();
+    });
+
+        // ESTO ESTÁ METIDO MÁS A SACO AÚN: DESACTIVAR EL TRIGGER AL CLICKAR FUERA DEL BOTÓN
+        $(document).click(function() {
+            $('.fxs_overflowOptions').removeClass("active");
+            $('.fxs_btn_group').removeClass("active");
+        });
+
+    
+    ///////////////////////////////////////////
+    /////////////// TYPE AHEAD ///////////////
+    /////////////////////////////////////////
+
+    // DATOS METIDOS A SACO!!!!
+
+    var data = {
+        topics: ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda",
+            "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh",
+            "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
+            "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burma",
+            "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Central African Republic", "Chad",
+            "Chile", "China", "Colombia", "Comoros", "Congo, Democratic Republic", "Congo, Republic of the",
+            "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti",
+            "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador",
+            "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon",
+            "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Greenland", "Grenada", "Guatemala", "Guinea",
+            "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India",
+            "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan",
+            "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kuwait", "Kyrgyzstan", "Laos",
+            "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
+            "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands",
+            "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Mongolia", "Morocco", "Monaco",
+            "Mozambique", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger",
+            "Nigeria", "Norway", "Oman", "Pakistan", "Panama", "Papua New Guinea", "Paraguay", "Peru",
+            "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Samoa", "San Marino",
+            "Sao Tome", "Saudi Arabia", "Senegal", "Serbia and Montenegro", "Seychelles", "Sierra Leone",
+            "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "Spain",
+            "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan",
+            "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey",
+            "Turkmenistan", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States",
+            "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"],
+        contributors: ["Abu Dhabi", "Abuja", "Accra", "Adamstown", "Addis Ababa", "Algiers", "Alofi", "Amman", "Amsterdam",
+            "Andorra la Vella", "Ankara", "Antananarivo", "Apia", "Ashgabat", "Asmara", "Astana", "Asunción", "Athens",
+            "Avarua", "Baghdad", "Baku", "Bamako", "Bandar Seri Begawan", "Bangkok", "Bangui", "Banjul", "Basseterre",
+            "Beijing", "Beirut", "Belgrade", "Belmopan", "Berlin", "Bern", "Bishkek", "Bissau", "Bogotá", "Brasília",
+            "Bratislava", "Brazzaville", "Bridgetown", "Brussels", "Bucharest", "Budapest", "Buenos Aires", "Bujumbura",
+            "Cairo", "Canberra", "Caracas", "Castries", "Cayenne", "Charlotte Amalie", "Chisinau", "Cockburn Town",
+            "Conakry", "Copenhagen", "Dakar", "Damascus", "Dhaka", "Dili", "Djibouti", "Dodoma", "Doha", "Douglas",
+            "Dublin", "Dushanbe", "Edinburgh of the Seven Seas", "El Aaiún", "Episkopi Cantonment", "Flying Fish Cove",
+            "Freetown", "Funafuti", "Gaborone", "George Town", "Georgetown", "Georgetown", "Gibraltar", "King Edward Point",
+            "Guatemala City", "Gustavia", "Hagåtña", "Hamilton", "Hanga Roa", "Hanoi", "Harare", "Hargeisa", "Havana",
+            "Helsinki", "Honiara", "Islamabad", "Jakarta", "Jamestown", "Jerusalem", "Juba", "Kabul", "Kampala",
+            "Kathmandu", "Khartoum", "Kiev", "Kigali", "Kingston", "Kingston", "Kingstown", "Kinshasa", "Kuala Lumpur",
+            "Kuwait City", "Libreville", "Lilongwe", "Lima", "Lisbon", "Ljubljana", "Lomé", "London", "Luanda", "Lusaka",
+            "Luxembourg", "Madrid", "Majuro", "Malabo", "Malé", "Managua", "Manama", "Manila", "Maputo", "Marigot",
+            "Maseru", "Mata-Utu", "Mbabane Lobamba", "Melekeok Ngerulmud", "Mexico City", "Minsk", "Mogadishu", "Monaco",
+            "Monrovia", "Montevideo", "Moroni", "Moscow", "Muscat", "Nairobi", "Nassau", "Naypyidaw", "N'Djamena",
+            "New Delhi", "Niamey", "Nicosia", "Nicosia", "Nouakchott", "Nouméa", "Nukuʻalofa", "Nuuk", "Oranjestad",
+            "Oslo", "Ottawa", "Ouagadougou", "Pago Pago", "Palikir", "Panama City", "Papeete", "Paramaribo", "Paris",
+            "Philipsburg", "Phnom Penh", "Plymouth Brades Estate", "Podgorica Cetinje", "Port Louis", "Port Moresby",
+            "Port Vila", "Port-au-Prince", "Port of Spain", "Porto-Novo Cotonou", "Prague", "Praia", "Cape Town",
+            "Pristina", "Pyongyang", "Quito", "Rabat", "Reykjavík", "Riga", "Riyadh", "Road Town", "Rome", "Roseau",
+            "Saipan", "San José", "San Juan", "San Marino", "San Salvador", "Sana'a", "Santiago", "Santo Domingo",
+            "São Tomé", "Sarajevo", "Seoul", "Singapore", "Skopje", "Sofia", "Sri Jayawardenepura Kotte", "St. George's",
+            "St. Helier", "St. John's", "St. Peter Port", "St. Pierre", "Stanley", "Stepanakert", "Stockholm", "Sucre",
+            "Sukhumi", "Suva", "Taipei", "Tallinn", "Tarawa Atoll", "Tashkent", "Tbilisi", "Tegucigalpa", "Tehran",
+            "Thimphu", "Tirana", "Tiraspol", "Tokyo", "Tórshavn", "Tripoli", "Tskhinvali", "Tunis", "Ulan Bator", "Vaduz",
+            "Valletta", "The Valley", "Vatican City", "Victoria", "Vienna", "Vientiane", "Vilnius", "Warsaw",
+            "Washington, D.C.", "Wellington", "West Island", "Willemstad", "Windhoek", "Yamoussoukro", "Yaoundé", "Yaren",
+            "Yerevan", "Zagreb"]
+    };
+
+    $('#q').typeahead({
+        minLength: 1,
+        order: "asc",
+        group: true,
+        groupMaxItem: 6,
+        hint: true,
+        dropdownFilter: false,
+        emptyTemplate: 'No result for "{{query}}"',
+        source: {
+            Topics: {
+                data: data.topics,
+                template: '<span class="fxs_typeaheadTxt">{{display}}<span class="fxs_typeaheadTxt">'
+            },
+            Contributors: {
+                data: data.contributors,
+                template: '<div class="fxs_avatar_circleImage_s"><img class="" src="https://s3.amazonaws.com/uifaces/faces/twitter/brad_frost/128.jpg"></div> <span class="fxs_typeaheadTxt">{{display}}</span>'
+            }
+        },
+        debug: true
+    });
+
+    ////////// Estados del list filter //////////
+    // Esto ESTÁ METIDO A SACO para que funcione el trigger del filtro
+    $('.fxs_btn_filter').click(function(evt) {
+        $('.fxs_filter').addClass("active");
+        evt.stopPropagation();
+    });  
+
+        // ESTO ESTÁ METIDO MÁS A SACO AÚN: DESACTIVAR EL TRIGGER AL CLICKAR FUERA
+        $('.fxs_dismissQuery').click(function() {
+            $('.fxs_filter').removeClass("active");
+            $('.fxs_typeaheadContainer').removeClass("result");
+            $('.fxs_typeaheadContainer').removeClass("hint");
+            $('.fxs_typeaheadContainer').removeClass("backdrop");
+            $('.fxs_queryResults').removeClass("fxs_selectedQuery");
+        });
+
+        // ESTO YA ES DE PENA PERO EJEMPLIFICA BIEN: ACTIVAR LA CLASE DEL FILTRO CON QUERY SELECCIONADA
+        $('.fxs_content_subnav').click(function() {
+            $('.fxs_queryResults').addClass("fxs_selectedQuery");
+        });
+});
